b3a15b5d81ad007e775eb52421068da46ccddef7
EricStockTeacher <ericstockteacher@gmail.com>
2024-11-19 10:02:20 -0330

    modified the joke code

--- jokes.py
+++ jokes.py
@@ -2,7 +2,11 @@
 import random
 
 
-
+def menu():
+    print("Welcome to the joke program")
+    print("Please enter a command")
+    print("list - list all jokes")
+    print("go - tell a random joke")
 
 
 
@@ -12,6 +16,7 @@ def main():
     jokes_list = [ ["What does a baby computer call his father?","Data"],
                    ["What do a tick and the Eiffel Tower have in common", "They're both Paris sites"]
                  ]
+    menu()
 
     joke = random.choice(jokes_list)
     print(joke)
